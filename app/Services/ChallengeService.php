71fccff78cb6ae91aca92150090499873ecfa61d
heritier24 <heritier.ganza@kudibooks.com>
2023-01-03 19:34:07 +0200

    created service of challenge services

---
+++ app/Services/ChallengeService.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Services;
+
+
+class ChallengeService
+{
+    public function listChallenge()
+    {
+        
+    }
+
+    public function createChallenge()
+    {
+
+    }
+
+    public function updateChallenge()
+    {
+
+    }
+}

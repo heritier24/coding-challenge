534323d9f8e06bfaf18f14d596efa52521ba7f4c
heritier24 <heritier.ganza@kudibooks.com>
2023-01-03 21:31:34 +0200

    list reports, update state of reports and creating reports message form the first time created

--- app/Services/ChallengeService.php
+++ app/Services/ChallengeService.php
@@ -1,21 +1,49 @@
 <?php
+
 namespace App\Services;
 
+use App\Models\Reports;
+use Illuminate\Support\Facades\DB;
 
 class ChallengeService
 {
-    public function listChallenge()
+    public function listReports(): array
     {
-        
+        $reports = DB::select("SELECT reports.id, reports.source, 
+                               reports.reportType, reports.message, 
+                               reports.state 
+                               FROM reports 
+                               WHERE reports.state = 'OPEN' ");
+
+        return $reports;
     }
 
-    public function createChallenge()
+    public function createdReports(string $reportType, string $message): Reports
     {
+        $postReports = Reports::create([
+            'reportType' => $reportType,
+            'message' => $message,
+        ]);
 
+        return $postReports;
     }
 
-    public function updateChallenge()
+    public function updateReports(int $id, string $state)
     {
+        $validateId = Reports::where('id', $id)->first();
 
+        if (is_null($validateId) || !$validateId) {
+
+            return false;
+
+        } else {
+
+            $updateReports = Reports::where('id', $id)->update([
+                
+                'state' => $state,
+            ]);
+        }
+
+        return $updateReports;
     }
 }

9dbd89ec397829877f867dac0b2fa8db3c5b7d1c
heritier24 <heritier.ganza@kudibooks.com>
2023-01-03 19:27:26 +0200

    env settings updated and created endpoints routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChallengeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('app')->group(function () {
+
+    Route::prefix('challenge')->group(function () {
+
+        Route::get('/list-messages-challenge', [ChallengeController::class, 'listChallenge']);
+
+        Route::post('/create-challenge-messages', [ChallengeController::class, 'createChallenge']);
+
+        Route::put('/update-challenge-status', [ChallengeController::class, 'updateChallenge']);
+
+    });
 });

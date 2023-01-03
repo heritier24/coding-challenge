a05f48d0148000d97fb72233ad18f4ca3bf25cb4
heritier24 <heritier.ganza@kudibooks.com>
2023-01-03 21:38:04 +0200

    migration modified well

--- database/migrations/2023_01_03_173819_create_reports_table.php
+++ database/migrations/2023_01_03_173819_create_reports_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
-            $table->string('source');    // REPORT ,  
+            $table->string('source')->default('REPORT');    // REPORT ,  
             $table->string('reportType');   // SPAM , VIOLATES_POLICIES, REPLY, .... 
             $table->string('message');
-            $table->string('reportId');
-            $table->string('referenceResourceId');   // id of reference resourcce id 
-            $table->string('referenceResourceType');       // POST , ARTICLE
-            $table->string('state');      // OPEN, CLOSED, RESOLVED
+            $table->string('reportId')->default('d9865001-c46e-4fd5-b810-4310fa41cf3d');
+            $table->string('referenceResourceId')->default('c8ba7a4b-1aa6-471b-bf91-748f915af1fb');   // id of reference resourcce id 
+            $table->string('referenceResourceType')->default('POST');       // POST , ARTICLE
+            $table->string('state')->default('OPEN');      // OPEN, CLOSED, RESOLVED
             $table->timestamps();
         });
     }
